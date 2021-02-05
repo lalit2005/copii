b66b9246e762be879044b0eb58d4fbafb704e168
Lalit <lalitvijay9480@gmail.com>
2021-02-05 22:14:38 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,26 @@
+let searchParams = {};
+
+decodeURI(window.location.search)
+	.replace("?", "")
+	.split("&")
+	.map((param) => param.split("="))
+	.reduce((values, [key, value]) => {
+		searchParams[key] = value;
+		return values;
+	}, {});
+
+console.log(searchParams);
+
+if (navigator.clipboard) {
+	navigator.clipboard.writeText(searchParams.asd).then(
+		// setInterval(() => {
+		// 	window.history.back();
+		// }, 1700)
+		console.log("hi")
+	);
+} else {
+	window.prompt(
+		"Sorry!! this website has no access to Clipbord API, please press `Ctrl/Cmd + C` and hit `Enter`",
+		searchParams
+	);
+}
