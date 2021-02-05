9daad60d1acc2115c5bdbd68f02e6291af3051ae
Lalit <lalitvijay9480@gmail.com>
2021-02-05 23:02:33 +0530

    Finish dark mode and bit refactoring

--- script/script.js
+++ script/script.js
@@ -12,15 +12,36 @@ decodeURI(window.location.search)
 console.log(searchParams);
 
 if (navigator.clipboard) {
-	navigator.clipboard.writeText(searchParams.asd).then(
-		setInterval(() => {
-			window.history.back();
-		}, 1700)
-	);
+	if (searchParams.ct) {
+		navigator.clipboard
+			.writeText(searchParams.ct)
+			.then(
+				setInterval(() => {
+					window.history.back();
+				}, 1700)
+			)
+			.catch((e) => {
+				window.prompt(
+					e +
+						"\n\nThis website has no access to Clipbord API, please press `Ctrl/Cmd + C` and hit `Enter`",
+					searchParams.ct
+				);
+				setInterval(() => {
+					window.history.back();
+				}, 1700);
+			});
+	} else {
+		document.querySelector("#text").innerHTML = "No text to copy! 😐😐😐";
+	}
 	console.log("hi");
 } else {
 	window.prompt(
 		"Sorry!! this website has no access to Clipbord API, please press `Ctrl/Cmd + C` and hit `Enter`",
 		searchParams
 	);
+}
+
+if (searchParams.tm && searchParams.tm == "blk") {
+	document.querySelector("body").style.backgroundColor = "black";
+	document.querySelector("body").style.color = "white";
 }
